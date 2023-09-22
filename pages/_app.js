bf02e97f662648503b8f51697ad0d4f8aee3ef7a
weissrot <wildeproust@gmail.com>
2023-09-22 17:17:04 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import React from 'react';
+import { Layout } from '../components';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
