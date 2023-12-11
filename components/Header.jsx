67c04205c0fd8428b53d996b2b6a89ac75b6118a
weissrot <wildeproust@gmail.com>
2023-12-11 21:58:09 +0100

    debug

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Yings Blog</span>
+            <span className="cursor-pointer font-bold text-4xl text-white">A Forest in Shire</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
